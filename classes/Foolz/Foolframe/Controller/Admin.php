dfc39d43b322b609894f6643610df99c75634a1c
Hung Tran <oohnoitz@gmail.com>
2013-03-19 20:43:17 -0500

    fixed admin controller uri::segment check

--- classes/Foolz/Foolframe/Controller/Admin.php
+++ classes/Foolz/Foolframe/Controller/Admin.php
@@ -12,7 +12,7 @@ class Admin extends Common
     {
 		parent::before();
 
-		if ( ! \Auth::has_access('maccess.user') && ! in_array(\Uri::string(), ['admin/account/login', 'admin/account/forgot_password', 'admin/account/register']))
+	if ( ! \Auth::has_access('maccess.user') && ! in_array(\Uri::segment(3), ['register', 'activate', 'login', 'change_password', 'forgot_password']))
 		{
 			return \Response::redirect('admin/account/login');
 		}
