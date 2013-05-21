66fd71a88f1911007350ed52ccec0dcf3c98545e
woxxy <woxxy@foolrulez.org>
2013-05-22 00:02:49 +0200

    Removing debug line

--- classes/Foolz/Foolframe/Model/Framework.php
+++ classes/Foolz/Foolframe/Model/Framework.php
@@ -95,7 +95,6 @@ class Framework extends HttpKernel
 			$request->attributes->add(['_controller' => $controller_404]);
 			$response = $this->handle($request);
 		}
-		var_dump(get_included_files());
 
 		$response->send();
 	}
