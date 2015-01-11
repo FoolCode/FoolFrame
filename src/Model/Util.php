e813e72cfe49ec8b16c405c24266a3da49e975d0
Woxxy <woxxap@gmail.com>
2015-01-11 14:00:48 +0100

    Adding recursive file delete, copy and size static methods in Util model

--- src/Model/Util.php
+++ src/Model/Util.php
@@ -3,6 +3,9 @@
 namespace Foolz\Foolframe\Model;
 
 
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
 class Util {
 
     /**
@@ -30,5 +33,98 @@ class Util {
             $randomString .= $characters[rand(0, strlen($characters) - 1)];
         }
         return $randomString;
+    }
+
+    /**
+     * Delete a file/recursively delete a directory
+     *
+     * NOTE: Be very careful with the path you pass to this!
+     *
+     * From: http://davidhancock.co/2012/11/useful-php-functions-for-dealing-with-the-file-system/
+     *
+     * @param string $path The path to the file/directory to delete
+     * @return void
+     */
+    public static function delete_recursive($path)
+    {
+        if (is_dir($path)) {
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::CHILD_FIRST
+            );
+
+            foreach ($iterator as $file) {
+                if ($file->isDir()) {
+                    rmdir($file->getPathname());
+                }
+                else {
+                    unlink($file->getPathname());
+                }
+            }
+
+            rmdir($path);
+        }
+        else {
+            unlink($path);
+        }
+    }
+
+    /**
+     * Copy a file or recursively copy a directories contents
+     *
+     * From: http://davidhancock.co/2012/11/useful-php-functions-for-dealing-with-the-file-system/
+     *
+     * @param string $source The path to the source file/directory
+     * @param string $dest The path to the destination directory
+     * @return void
+     */
+    public static function copy_recursive($source, $dest)
+    {
+        if (is_dir($source)) {
+            $iterator = new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS),
+                RecursiveIteratorIterator::SELF_FIRST
+            );
+
+            foreach ($iterator as $file) {
+                if ($file->isDir()) {
+                    mkdir($dest.DIRECTORY_SEPARATOR.$iterator->getSubPathName());
+                }
+                else {
+                    copy($file, $dest.DIRECTORY_SEPARATOR.$iterator->getSubPathName());
+                }
+            }
+        }
+        else {
+            copy($source, $dest);
+        }
+    }
+
+    /**
+     * Return the size of a file or a directory and its contents in bytes
+     *
+     * NOTE: This function may return unexpected results for files larger than
+     *       2GB on 32bit hosts due to PHP's integer type being 32bit signed.
+     *
+     * From: http://davidhancock.co/2012/11/useful-php-functions-for-dealing-with-the-file-system/
+     *
+     * @param string $path The path to the file/directory to calculate the size of
+     * @return int
+     */
+    public static function size_recursive($path)
+    {
+        $size = 0;
+        if (is_dir($path)) {
+            $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+
+            foreach ($iterator as $file) {
+                $size += $file->getSize();
+            }
+        }
+        else {
+            $size = filesize($path);
+        }
+
+        return $size;
     }
 }
